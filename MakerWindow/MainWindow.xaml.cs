54f70776d9fa1fe8fa5e0e7f1ea240862d7d8d2f
Tim(Shadow) <timsnedden138@gmail.com>
2019-02-28 14:28:13 -0800

    Modifying the JSONS and implementation of buttons

--- MakerWindow/MainWindow.xaml.cs
+++ MakerWindow/MainWindow.xaml.cs
@@ -26,20 +26,26 @@ namespace MakerWindow
         public ObservableCollection<string> added = new ObservableCollection<string>();
         public ObservableCollection<Recipe> ValidRecipes = new ObservableCollection<Recipe>();
         public ObservableCollection<string> addedRec = new ObservableCollection<string>();
+        public ObservableCollection<RecipeSteps> Steps = new ObservableCollection<RecipeSteps>();
+        public ObservableCollection<string> addedRecsteps = new ObservableCollection<string>();
         public MainWindow()
         {
-            
+
             InitializeComponent();
-            string[] linesIng = System.IO.File.ReadAllLines(@"C:\Users\s189066\source\repos\RecipeMaker\MakerWindow\Ingredients.txt");
-            string[] linesRec = System.IO.File.ReadAllLines(@"C:\Users\s189066\source\repos\RecipeMaker\MakerWindow\Recipes.txt");
+            Recipes outRec = JsonConvert.DeserializeObject<Recipes>(File.ReadAllText(@"C:\Users\s189066\source\repos\RecipeMaker\MakerWindow\rec.json"));
+            Ingredients outIng = JsonConvert.DeserializeObject<Ingredients>(File.ReadAllText(@"C:\Users\s189066\source\repos\RecipeMaker\MakerWindow\ing.json"));
             Adding.ItemsSource = added;
             Ingredients.ItemsSource = possibleIngs;
             Addingrec.ItemsSource = addedRec;
-            foreach (string line in linesRec)
+            RecipeSteps.ItemsSource = Steps;
+            for (int i = 0; i < outIng.Ings.Count(); i++)
             {
-                ValidRecipes.Add(new Recipe(line, new List<Ingredient>() { new Ingredient("Butter", false), new Ingredient("Eggs", false) }));
+                possibleIngs.Add(new Ingredient(outIng.Ings[i].FoodName, outIng.Ings[i].IsUsed));
             }
-            ValidRecipes.Add(new Recipe("Toasty", new List<Ingredient>() { new Ingredient("Cheese", false), new Ingredient("Toast", false) }));
+            for (int r = 0; r < outRec.Recs.Count(); r++)
+            {
+                ValidRecipes.Add(new Recipe(outRec.Recs[r].RecipeName, outRec.Recs[r].RequiredIngs));
+            }
         }
         private void HandleCheckChanged(object sender, RoutedEventArgs e)
         {
@@ -75,15 +81,31 @@ namespace MakerWindow
                     }
                 }
             }
-            //if (checkedIngs == ValidRecipes.Count)
-            //{
-            //    addedRec.Add(ValidRecipes[recipeCounter].RecipeName);
-            //}
         }
         private void Adding_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Steps.Add(new RecipeSteps("NAMES\nTesting"));
+        }
+    }
+
+
+    public class RecipeSteps
+    {
+        public RecipeSteps()
+        {
+
+            Stepstomake = "not food";
+        }
+        public RecipeSteps(string _Stepstomake)
+        {
+            Stepstomake = _Stepstomake;
+        }
+        public string Stepstomake { get; set; }
     }
 
     public class Ingredient
@@ -92,7 +114,7 @@ namespace MakerWindow
         {
 
             FoodName = "not food";
-            IsUsed = true;
+            IsUsed = false;
         }
         public Ingredient(string name, bool use)
         {
@@ -119,5 +141,14 @@ namespace MakerWindow
         public string RecipeName { get; set; }
         public List<Ingredient> RequiredIngs { get; set; }
 
+    }
+    public class Ingredients
+    {
+        public Ingredient[] Ings { get; set; }
+    }
+
+    public class Recipes
+    {
+        public Recipe[] Recs { get; set; }
     }
 }
